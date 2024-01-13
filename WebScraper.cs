63c13df1f8b2d7d56ee598e523129b983b9a4a30
Your Name <aschena.44@gmail.com>
2024-01-13 06:54:05 -0700

    Konvoy, Naavik, EGD

---
+++ WebScraper.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HtmlAgilityPack; //HTML parser, a tool designed to read and interpret HTML (HyperText Markup Language) documents. 
+using System;
+using System.Net.Http;
+using static System.Net.WebRequestMethods;
+
+namespace WebScraper
+{
+    class WebScraper
+    {
+        static void Main()
+    {
+            KonvoyWebScraper.Run();
+            NaavikDigestWebScraper.Run();
+            EliteGameDeveloperWebScraper.Run();
+    }
+    }
+}
